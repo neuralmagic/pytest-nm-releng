589342d0c30db70ecb976242dd4cf730de1cb2ca
Domenic Barbuzzi <domenic@neuralmagic.com>
2025-03-13 19:02:03 -0400

    Drop non-functional features (#4)
    * Drop non-functional features
    
    * README style fix

--- src/pytest_nm_releng/plugin.py
+++ src/pytest_nm_releng/plugin.py
@@ -31,31 +31,10 @@ def generate_junit_flags() -> list[str]:
     if prefix := os.getenv("NMRE_JUNIT_PREFIX"):
         junitxml_file = junitxml_file.with_name(f"{prefix}{junitxml_file.name}")
 
-    return [f"--junit-xml={junitxml_file.as_posix()}"]
-
-
-def generate_full_junit_flags() -> list[str]:
-    if os.environ.get("NMRE_JUNIT_FULL", "") == "1":
-        return ["-o", "junit_logging=all", "-o", "junit_log_passing_tests=True"]
-
-    return []
-
-
-def generate_coverage_flags() -> list[str]:
-    if not (cc_package_name := os.getenv("NMRE_COV_NAME")):
-        return []
-
-    return [
-        f"--cov={cc_package_name}",
-        "--cov-append",
-        "--cov-report=html:coverage-html",
-        "--cov-report=json:coverage.json",
-    ]
+    return [f"--junit-xml={junitxml_file}"]
 
 
 def pytest_load_initial_conftests(early_config, args: list[str], parser):
     new_args: list[str] = []
     new_args.extend(generate_junit_flags())
-    new_args.extend(generate_full_junit_flags())
-    new_args.extend(generate_coverage_flags())
     args[:] = [*args, *new_args]
