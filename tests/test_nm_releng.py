a538ea56200474d781fafdddbcb07a22ea044452
Domenic Barbuzzi <dbarbuzz@redhat.com>
2025-03-14 15:52:49 -0400

    Augment test to validate correctly named file

--- tests/test_nm_releng.py
+++ tests/test_nm_releng.py
@@ -12,8 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+import re
 from importlib.metadata import version
+from pathlib import Path
 from xml.dom import minidom
 
 import pytest
@@ -39,11 +40,33 @@ def test_plugin_adds_junit_args(
     """Verify the plugin adds the expected junit args"""
 
     monkeypatch.setenv("NMRE_JUNIT_BASE", "results")
+    monkeypatch.setenv("NMRE_JUNIT_PREFIX", "report-")
 
+    # parse pytest configuration and make sure it sets the flag
     cf = pytester.parseconfigure()
-    actual = cf.getoption("--junit-xml", None)
+    actual = cf.getoption("--junit-xml")
     assert isinstance(actual, str)
     assert actual.startswith("results")
+
+    # run pytest and make sure it generates an XML file
+    pytester.makepyfile("""
+        def test_pass():
+            pass
+    """)
+    result = pytester.runpytest()
+    assert "generated xml file:" in result.stdout.str()
+
+    # make sure the generated XML file is named correctly and exists
+    pattern = re.compile(r"generated xml file: (.*?(?:\.xml))")
+    match = pattern.search(result.stdout.str())
+    assert match is not None
+    xml_file = Path(match[1])
+    assert xml_file.exists()
+    assert xml_file.parent.name == "results"
+    assert xml_file.name.startswith("report-")
+    assert re.compile(r"\d{10,}\.\d+\.xml").fullmatch(
+        xml_file.name.removeprefix("report-")
+    )
 
 
 @pytest.mark.parametrize("properties", [["courses=3"], ["courses=3", "dessert=true"]])
