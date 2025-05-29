fb975a4a01ee4a9544eac99030c977bd870969af
Debolina Roy <debroy@redhat.com>
2025-05-29 11:54:52 -0400

    coverage -10

--- src/pytest_nm_releng/plugin.py
+++ src/pytest_nm_releng/plugin.py
@@ -89,13 +89,6 @@ def generate_coverage_flags() -> list[str]:
         "--cov-report=term --cov-report=html --cov-report=json"
     )
 
-    existing_addopts = ini_options.get("addopts", "")
-    if coverage_opts not in existing_addopts:
-        ini_options["addopts"] = f"{existing_addopts} {coverage_opts}".strip()
-
-        with open(pyproject_path, "w") as f:
-            toml.dump(data, f)
-
     return [
         f"--cov={cc_package_name}",
         "--cov-append",
