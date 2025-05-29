c14ef9b5f0d89607fac1601b390cacef21c96c3e
Debolina Roy <debroy@redhat.com>
2025-05-29 13:36:43 -0400

    coverage -19

--- src/pytest_nm_releng/plugin.py
+++ src/pytest_nm_releng/plugin.py
@@ -91,11 +91,11 @@ def generate_coverage_flags() -> list[str]:
     )
 
     existing_addopts = ini_options.get("addopts", "")
-    # if coverage_opts not in existing_addopts:
-    #     ini_options["addopts"] = f"{existing_addopts} {coverage_opts}".strip()
+    if coverage_opts not in existing_addopts:
+        ini_options["addopts"] = f"{existing_addopts} {coverage_opts}".strip()
 
-    #     with open(pyproject_path, "w") as f:
-    #         toml.dump(data, f)
+        with open(pyproject_path, "w") as f:
+            toml.dump(data, f)
 
     return [
         f"--cov={cc_package_name}",
