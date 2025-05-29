c37ef4142d47af13a009a6735e4a8f5cfd6475ee
Debolina Roy <debroy@redhat.com>
2025-05-29 14:03:09 -0400

    coverage -20

--- src/pytest_nm_releng/plugin.py
+++ src/pytest_nm_releng/plugin.py
@@ -96,6 +96,7 @@ def generate_coverage_flags() -> list[str]:
 
         with open(pyproject_path, "w") as f:
             toml.dump(data, f)
+        print(toml.dumps(data))
 
     return [
         f"--cov={cc_package_name}",
