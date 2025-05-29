a08e91ee22d79bdbb8d47fcba705eabf85a3a6ab
Debolina Roy <debroy@redhat.com>
2025-05-29 12:22:10 -0400

    coverage -12

--- src/pytest_nm_releng/plugin.py
+++ src/pytest_nm_releng/plugin.py
@@ -85,7 +85,7 @@ def generate_coverage_flags() -> list[str]:
     ini_options = pytest_section.setdefault("ini_options", {})
 
     coverage_opts = (
-        f"--cov={cc_package_name} --cov-append "
+        f"--cov=vllm --cov-append "
         "--cov-report=term --cov-report=html --cov-report=json"
     )
 
